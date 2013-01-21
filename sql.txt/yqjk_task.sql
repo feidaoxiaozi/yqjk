422b70b1a3dbdc928aebd4f1befbef1b22a88ff1
feidaoxiaozi <ww3179367@163.com>
2013-01-21 15:08:42 +0800

    recommit

---
+++ sql.txt/yqjk_task.sql
@@ -0,0 +1,32 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: yqjk
+Target Host: localhost
+Target Database: yqjk
+Date: 2013-1-21 15:06:59
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for yqjk_task
+-- ----------------------------
+DROP TABLE IF EXISTS `yqjk_task`;
+CREATE TABLE `yqjk_task` (
+  `roleId` int(10) NOT NULL auto_increment,
+  `roleName` varchar(40) NOT NULL,
+  `newsResource` varchar(30) NOT NULL,
+  `findStyle` varchar(30) NOT NULL,
+  `collectDate` varchar(20) NOT NULL,
+  `collectCondition` varchar(200) NOT NULL,
+  `contentMate` varchar(50) default NULL,
+  `Site_collection` varchar(20) default NULL,
+  `roleState` varchar(10) NOT NULL,
+  PRIMARY KEY  (`roleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 4096 kB';
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `yqjk_task` VALUES ('1', '2013春晚', '新闻', '标题&时间搜索', '当天', '赵本山', '小品', '启用', '启用');
+INSERT INTO `yqjk_task` VALUES ('2', '中日关系', '新闻', '正文&相关度搜索', '当天', '丰富', '中方', '启用', '启用');
---
+++ sql.txt/yqjk_user.sql
@@ -0,0 +1,34 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: yqjk
+Target Host: localhost
+Target Database: yqjk
+Date: 2013-1-21 15:07:09
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for yqjk_user
+-- ----------------------------
+DROP TABLE IF EXISTS `yqjk_user`;
+CREATE TABLE `yqjk_user` (
+  `userId` int(8) NOT NULL auto_increment,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `age` int(8) NOT NULL,
+  PRIMARY KEY  (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `yqjk_user` VALUES ('1', 'tom', '111', '4234', '23');
+INSERT INTO `yqjk_user` VALUES ('3', 'wangyi', '111', 'tert', '23');
+INSERT INTO `yqjk_user` VALUES ('4', 'shenqi', '123456', 'rwer', '12');
+INSERT INTO `yqjk_user` VALUES ('5', 'wuqilong', '111', 'rtert', '123123');
+INSERT INTO `yqjk_user` VALUES ('6', 'liudehua', '123456', 'ww56575@163.com', '34');
+INSERT INTO `yqjk_user` VALUES ('7', 'james', '123456', 'fdsfs', '34');
+INSERT INTO `yqjk_user` VALUES ('8', 'dfgdf', '111', 'tyryy', '32');
+INSERT INTO `yqjk_user` VALUES ('9', 'ewq', '123', 'wwfdsf@1434.com', '34');
